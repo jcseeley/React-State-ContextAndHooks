241d11d59918d37385e3cc784341ebd39835afea
Jase Seeley <jcseeley31590@yahoo.com>
2022-06-01 09:16:19 -0700

    Add base files.

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../logo.svg";
+import { AuthContext } from "../App";
+
+export const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="card">
+        <div className="container">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
